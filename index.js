23490aa918b8689f37e5d37db30ad5f9a17761bd
Victor Pettersson <victorpettersson12345@gmail.com>
2018-07-14 09:37:53 +0200

    cors

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const morgan = require('morgan');
 const app = express();
 const router = require('./router');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 // db setup
 
@@ -15,6 +16,7 @@ mongoose.connect('mongodb://localhost:27017/auth', {
 
 // app setup
 app.use(morgan('combined'));
+app.use(cors());
 app.use(bodyParser.json({ type: '*/*' }));
 router(app);
 
